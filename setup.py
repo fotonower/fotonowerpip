ca3be48e59117a231b00ef24922abd90f37b818d
rahani <mhand_rahani@yahoo.fr>
2020-01-29 18:39:26 +0100

    https://github.com/fotonower/fotonowerpip/issues/2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='fotonower',  # Required
+    name='ftn',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -39,7 +39,8 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.14',  # Required
+    version='0.9.17',  # Required
+    author="victor@fotonower.com",
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -72,7 +73,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/pypa/sampleproject',  # Optional
+    url='https://github.com/fotonower/fotonowerpip',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
