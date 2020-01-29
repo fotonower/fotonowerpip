57ed0238f3c2733cf97100695c46859c24504d00
Victor Reutenauer <inscription@moilerat.com>
2020-01-29 19:01:12 +0100

    https://github.com/fotonower/API/issues/701

--- doc/example/upload_photo_with_datou.py
+++ doc/example/upload_photo_with_datou.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import fotonower as FC
+import fotonower as ftn
 import os
 
 if __name__ == "__main__":
@@ -12,10 +12,13 @@ if __name__ == "__main__":
                       help="folder where are photo to upload")
     parser.add_option("-t", "--token", action="store", type="string", dest="token",
                       default="", help=" token ")
-    parser.add_option("-u", "--root_url", action="store", type="string", dest="root_url", default="vision.fotonower.com",
+    parser.add_option("-u", "--root_url", action="store", type="string", dest="root_url", default="jussieu.fotonower.com",
                       help="root_url to upload photos")
     parser.add_option("-d", "--datou", action="store", type="string", dest="datou",
-                      default="2",help="datou id to be treated")
+                      default="1585",help="datou id to be treated")
+    parser.add_option("-i", "--is_live", action="store_true", dest="is_live",
+                      default=False, help="launch treatment datou immediatly")
+
     parser.add_option("-P", "--protocol", action="store", type="string", dest="protocol",
                       default="https", help="http or https")
     (x, args) = parser.parse_args()
@@ -24,7 +27,7 @@ if __name__ == "__main__":
         print("please provide a valid token")
         exit(1)
     try:
-        fc = FC.FotonowerConnect(x.token, x.root_url, x.protocol)
+        fc = ftn.FotonowerConnect(x.token, x.root_url, x.protocol)
     except :
         print("please provide a valid token")
         exit(1)
@@ -33,10 +36,10 @@ if __name__ == "__main__":
         exit(2)
     files = [x.file]
 
-    map_result_insert_aux,list_current_datou_ids = fc.upload_medias(files, upload_small=True,
-                                                 verbose=True, compute_classification=True, arg_aux={'classification_actions':"{'list_datou_ids':["+str(x.datou)+"], 'is_live' : true}"})
+    map_result_insert_aux, list_current_datou_ids = fc.upload_medias(files, list_datou_ids=[x.datou], is_live=is_live, upload_small=True, verbose=True)
 
     print(map_result_insert_aux)
+    print(map_result_insert_aux["res_json"])
     #{'mtr_datou_id':9,'is_live':true,'list_datou_ids_unused':[19],"manual_labelling":{"manual":456,"hashtag_type":451}}
     
     # to get results, may take some time depending on treatment asked
